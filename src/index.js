52d3a9f2cbc8f709a4be1d315a151962afcde1d8
NikolasMelui <slak@samaradom.ru>
2018-04-19 15:58:17 +0400

    Add graphql server and graphiql ui. Add new data.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const curSchema = buildSchema(`
+	type Query {
+		hello: String
+		number: Int
+	}
+`);
+
+const root = {
+	hello: () => 'Hello, Rinat!',
+	number: () => 123 + 123,
+};
 
 const server = express();
+
+server.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: curSchema,
+		rootValue: root,
+		graphiql: true,
+	})
+);
 
 server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
